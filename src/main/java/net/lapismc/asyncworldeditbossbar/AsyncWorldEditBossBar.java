acf52dfb66c4edf4de05818083697ea792f064a5
Benjamin <DNAblue2112@gmail.com>
2020-03-28 16:59:38 +1030

    Minor addition to 1.0.0

--- src/main/java/net/lapismc/asyncworldeditbossbar/AsyncWorldEditBossBar.java
+++ src/main/java/net/lapismc/asyncworldeditbossbar/AsyncWorldEditBossBar.java
@@ -1,6 +1,7 @@
 package net.lapismc.asyncworldeditbossbar;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.boss.BarColor;
 import org.bukkit.boss.BarStyle;
 import org.bukkit.boss.BossBar;
@@ -21,44 +22,64 @@ public final class AsyncWorldEditBossBar extends JavaPlugin implements IProgress
 
     @Override
     public void onEnable() {
+        //Save the config before the registration to make sure the variables there in will be available
         saveDefaultConfig();
+        //Add this class as a progress display in the AsyncWorldEdit API
         AsyncWorldEditMain.getInstance().getAPI().getProgressDisplayManager().registerProgressDisplay(this);
         getLogger().info(getName() + " v." + getDescription().getVersion() + " has been enabled");
     }
 
     @Override
     public void onDisable() {
+        //Remove this class as a progress display in the AsyncWorldEdit API
         AsyncWorldEditMain.getInstance().getAPI().getProgressDisplayManager().unregisterProgressDisplay(this);
     }
 
     @Override
     public void disableMessage(IPlayerEntry player) {
+        //Ignore this if we dont have a bar for this player
         if (!bossBars.containsKey(player.getUUID()))
             return;
-        bossBars.get(player.getUUID()).removePlayer(Objects.requireNonNull(Bukkit.getPlayer(player.getUUID())));
-        bossBars.remove(player.getUUID());
+        //Get the players bar
+        BossBar bossBar = bossBars.get(player.getUUID());
+        //Set it to invisible
+        bossBar.setVisible(false);
     }
 
     @Override
     public void setMessage(IPlayerEntry player, int jobsCount, int queuedBlocks, int maxQueuedBlocks, double timeLeft,
                            double placingSpeed, double percentage) {
+        //Get the stored boss bar or make a new one if we dont have one stored
         BossBar bossBar;
         if (bossBars.containsKey(player.getUUID())) {
             bossBar = bossBars.get(player.getUUID());
         } else {
-            bossBar = Bukkit.createBossBar("", BarColor.BLUE, BarStyle.SOLID);
+            //Make it the color defined in the config or blue if they haven't set it
+            BarColor color = BarColor.valueOf(getConfig().getString("BarColor", "BLUE"));
+            bossBar = Bukkit.createBossBar("", color, BarStyle.SOLID);
+            //Add the player to the bar, The title should be set so quickly that they shouldn't see the first update anyway
+            bossBar.addPlayer(Objects.requireNonNull(Bukkit.getPlayer(player.getUUID())));
         }
+        //The number we get is 0-100, we need 0-1, a simple /100 sorts that out
         bossBar.setProgress(percentage / 100);
+        //The doubles given to us are stupidly long, so we use this number format to shorten them
         NumberFormat nf = new DecimalFormat("#.##");
         String format = getConfig().getString("TitleFormat");
+        //Translate colors and set a default
+        format = ChatColor.translateAlternateColorCodes('&',
+                format == null ? "ETA: $timeLeft seconds, Speed: $placingSpeed block/sec, $percentage %" : format);
+        //Swap in the variables
         format = format.replace("$jobsCount", jobsCount + "")
                 .replace("$queuedBlocks", queuedBlocks + "")
                 .replace("$maxQueuedBlocks", maxQueuedBlocks + "")
                 .replace("$timeLeft", nf.format(timeLeft))
                 .replace("$placingSpeed", nf.format(placingSpeed))
                 .replace("$percentage", nf.format(percentage));
+        //Set the title
         bossBar.setTitle(format);
-        bossBar.addPlayer(Objects.requireNonNull(Bukkit.getPlayer(player.getUUID())));
+        //Show it to the player
+        bossBar.setVisible(true);
+        //Store it for later
         bossBars.put(player.getUUID(), bossBar);
     }
 }
